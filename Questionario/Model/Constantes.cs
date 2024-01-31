4aac2a1b140569f778a5248e521801470f10533f
Gabriel N <133405896+Robotgamer324@users.noreply.github.com>
2024-01-31 11:50:25 -0300

    Inicio da coneccção do SQL

---
+++ Questionario/Model/Constantes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Constantes
+    {
+        public static string stringDeConexao = @"Data Source=.LAB5-06\SQLEXPRESS2022;Initial Catalog=Question-rio;User ID=SA;Password=Senailab05";
+        public static string StringDeConexao
+        {
+            get 
+            { 
+                return stringDeConexao; 
+            }
+        }
+    }
+}
